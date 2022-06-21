8bc93df5719986ab4dc89eb2ce4e004f7d607ba2
Jamall Malik <mimsjamall79@gmail.com>
2022-06-20 23:46:44 -0400

    not finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import axios from 'axios';
+import React from 'react';
+import Forms from 'Forms';
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      city: '',
+      cityData: {}
+    }
+  }
+  handleCityInput = (e) => {
+    this.setState = ({
+      city: e.target.value
+    });
+  };
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  handleCitySubmit = async (e) => {
+    e.preventDefault();
+    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
+    let cityInfo = await axios.get(url)
+  }
 }
 
+render() {
+
+  return (
+    <>
+      <Forms handleCityInput={this.handleCityInput}
+        handleSubmit={this.handleCitySubmit}/>
+
+    </>
+  );
+}
 export default App;
